4fceabd1abc1ec59cde8a812acdef1ee4ca03a98
GNVRLY <m.karwacikk@gmail.com>
2025-12-10 21:34:01 +0100

    basic widoki

--- learning/views.py
+++ learning/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Course, Lesson
+
+
+class CourseListView(ListView):
+    model = Course
+    template_name = "learning/course_list.html"
+    context_object_name = "courses"
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = "learning/course_detail.html"
+    context_object_name = "course"
+
+
+class LessonDetailView(DetailView):
+    model = Lesson
+    template_name = "learning/lesson_detail.html"
+    context_object_name = "lesson"
